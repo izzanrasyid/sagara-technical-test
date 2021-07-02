80729cfdef430bdaf748ee460b9f77f3bc3f48c9
Muhamad Irfan Fani <izzanrasyid31@gmail.com>
2021-07-02 18:14:07 +0700

    cleaning code

--- src/hooks/useHook.jsx
+++ src/hooks/useHook.jsx
@@ -2,16 +2,14 @@ import { useState } from "react"
 
 const useHook = () => {
   const [provinces] = useState([{
-    id: 1,
-    province: "DKI Jakarta",
-    cities: ["Jakarta Pusat", "Jakarta Selatan", "Jakarta Utara", "Jakarta Timur", "Jakarta Barat"]
+      id: 1,
+      province: "DKI Jakarta",
+      cities: ["Jakarta Pusat", "Jakarta Selatan", "Jakarta Utara", "Jakarta Timur", "Jakarta Barat"]
     }, {
       id: 2,
       province: "Jawa Barat",
       cities: ["Bandung", "Bekasi", "Cirebon", "Sukabumi", "Bogor"]
-    }
-
-    ])
+    }])
 
   return provinces
 }
